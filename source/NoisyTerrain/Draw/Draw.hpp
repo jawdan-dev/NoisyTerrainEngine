e96e8a2b60288c82e62d7b14495ed1ab46ae14c4
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-02-23 17:24:48 +0000

    Getters added.

--- source/NoisyTerrain/Draw/Draw.hpp
+++ source/NoisyTerrain/Draw/Draw.hpp
@@ -18,6 +18,11 @@ public:
 	~DrawManager();
 
 public:
+	J_GETTER_DIRECT(getProjection, m_projection, Matrix4&);
+	J_GETTER_DIRECT(getView, m_view, Matrix4&);
+	J_GETTER_DIRECT(getViewProjection, m_viewProjection, Matrix4&);
+
+public:
 	J_SETTER_DIRECT(setProjection, const Matrix4& projection, m_projection = projection; updateViewProjection());
 	J_SETTER_DIRECT(setView, const Matrix4& view, m_view = view; updateViewProjection());
 
