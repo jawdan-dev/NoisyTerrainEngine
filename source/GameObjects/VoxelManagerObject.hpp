6c5df526d1ea05a6194fabf8257a27f9005d3d6a
jawdan-dev <34403286+jawdan-dev@users.noreply.github.com>
2024-03-05 23:42:33 +0000

    View projection set in renderer.

--- source/GameObjects/VoxelManagerObject.hpp
+++ source/GameObjects/VoxelManagerObject.hpp
@@ -50,7 +50,6 @@ public:
 		m_voxelManager.getChunkManager()->process(playerLocation, m_terrainShader);
 
 		// Get shader uniforms.
-		const ShaderUniform* const u_viewProjection = m_terrainShader.getUniform("u_viewProjection");
 		const ShaderUniform* const u_playerPosition = m_terrainShader.getUniform("u_playerPosition");
 		const ShaderUniform* const u_renderDistance = m_terrainShader.getUniform("u_renderDistance");
 		const ShaderUniform* const u_voxelTexture = m_terrainShader.getUniform("u_voxelTexture");
@@ -58,7 +57,6 @@ public:
 
 		// Update shader uniforms..
 		glUseProgram(m_terrainShader.getProgram());
-		if (u_viewProjection != nullptr) glUniformMatrix4fv(u_viewProjection->m_location, 1, GL_FALSE, Draw.getViewProjection().getData());
 		if (u_playerPosition != nullptr) glUniform3f(u_playerPosition->m_location, activePosition.x(), activePosition.y(), activePosition.z());
 		if (u_renderDistance != nullptr) glUniform1f(u_renderDistance->m_location, Math::max<float>((voxelChunkViewDistance - 1) * Math::min(voxelChunkSizeX, voxelChunkSizeZ), 1.0f));
 		if (u_voxelTexture != nullptr) glUniform1i(u_voxelTexture->m_location, atlasTextureSlot);
